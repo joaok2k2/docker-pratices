37f154132c07f0e7c1c29c30c4f25c9aeab8ec4e
JoaoSilvaBSP <joao.silva@bspcloud.com>
2023-03-16 17:08:33 -0300

    Network host start

--- network/external_network_type/app.py
+++ network/external_network_type/app.py
@@ -9,9 +9,15 @@ app.config["DEBUG"] = True
 @app.route("/", methods=["GET"])
 
 def index():
-    data = rquests.get('https://randomuser.me/api')
+    data = requests.get('https://randomuser.me/api')
    
     return data.json()
 
 if __name__ == "__main__" :
     app.run(host="0.0.0.0", debug=True, port="5000")
+
+
+
+
+
+
